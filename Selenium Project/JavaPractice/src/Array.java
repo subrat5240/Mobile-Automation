65c5dcc8717edf3f65a05a0651b7fc70ba57c418
subrat5240 <subratrath89@mail.com>
2019-09-02 11:55:30 +0530

    Java practice code

---
+++ Selenium Project/JavaPractice/src/Array.java
@@ -0,0 +1,22 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int sum =0;
+		int c[]= {1,2,3,4,5,6,7,8,9};
+		
+		for (int i=0;i<c.length;i++ )
+		{
+			System.out.println(c[i]);
+			if (c[i]==7)
+			{
+				break;
+			}
+		}
+		
+		
+	}
+
+}
---
+++ Selenium Project/JavaPractice/src/ReverseString.java
@@ -0,0 +1,19 @@
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String t="";
+		for(int i=s.length()-1; i>=0;i--)
+		{
+			t=t+ s.charAt(i);
+		}
+		
+		System.out.println(t);
+		
+		
+	}
+
+}
