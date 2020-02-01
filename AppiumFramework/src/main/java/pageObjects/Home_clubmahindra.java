7e678c914c3e0f6b7d00d1cc8ec86bcc6932f8db
subrat5240 <subratrath89@mail.com>
2020-02-02 01:02:10 +0530

    Updated files

--- AppiumFramework/src/main/java/pageObjects/Home_clubmahindra.java
+++ AppiumFramework/src/main/java/pageObjects/Home_clubmahindra.java
@@ -1,8 +1,10 @@
 package pageObjects;
 import java.util.Properties;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 public class Home_clubmahindra {
@@ -14,7 +16,7 @@ public class Home_clubmahindra {
                     this.driver = driver;
                     PageFactory.initElements(driver, this);
     }
-    @FindBy(className="ui_icon search brand-global-nav-action-search-Search__icon--2DVjd")
+    @FindBy(xpath="//div[@class='brand-header-GlobalNavBar__searchWrap--35nok']")
     WebElement clikonSearch;
     
     @FindBy(xpath="//input[@id='mainSearch']") 
@@ -23,6 +25,14 @@ public class Home_clubmahindra {
     @FindBy(linkText="Write a review") 
     WebElement writeReview;
     
+    @FindBy(xpath="//div[@class='result-title']") 
+    WebElement clickhotel;
+    
+    @FindBy(xpath="//*[@id='bubble_rating']") 
+    WebElement rating;
+    
+    @FindBy(xpath="//div[@class='ui_bubble_rating_fl_bubble_50']") 
+    WebElement ratingpoint;
 	//////////////////////////////////////////////////////////////////
     public WebElement clikonSearch()
     {
@@ -37,6 +47,22 @@ public class Home_clubmahindra {
     public WebElement writeReview()
     {
                     return writeReview; // 
+                    
+    }
+    public WebElement clickhotel()
+    {
+                    return clickhotel; // 
+                    
+    }
+    public WebElement rating()
+    {
+    	Actions a = new Actions(driver);
+    a.moveToElement(rating).build().perform();
+                    return rating;
+    }
+    public WebElement ratingpoint()
+    {
+                    return ratingpoint; // 
                     
     }
     
--- AppiumFramework/src/test/java/practice/AppiumFramework/Zestmoney.java
+++ AppiumFramework/src/test/java/practice/AppiumFramework/Zestmoney.java
@@ -5,9 +5,11 @@ import java.util.Iterator;
 import java.util.Properties;
 import java.util.Set;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -23,25 +25,28 @@ public class Zestmoney {
 		FileInputStream fis=new FileInputStream("F:\\MyProjects\\AppiumFramework\\src\\main\\java\\practice\\AppiumFramework\\global.properties");
 		prop.load(fis);
 		
-		System.setProperty("webdriver.chrome.driver", "F:\\MyProjects\\AppiumFramework\\Drivers\\chromedriver.exe");
-		driver=new ChromeDriver();
+		System.setProperty("webdriver.gecko.driver", "F:\\MyProjects\\AppiumFramework\\Drivers\\geckodriver.exe");
+		driver=new FirefoxDriver();
 		
 		Home_clubmahindra fc = new Home_clubmahindra(driver);
 		 driver.get(prop.getProperty("url"));
 		  Thread.sleep(8000);
 		driver.manage().window().maximize();
 		fc.clikonSearch().click();
-		fc.mainSearch().sendKeys("Club Mahindra");
-		fc.mainSearch().sendKeys(Keys.DOWN, Keys.RETURN);
-		fc.writeReview().click();
+		fc.mainSearch().sendKeys("Club Mahindra",Keys.RETURN);
+		Thread.sleep(5000);
+		fc.clickhotel().click();
+		Thread.sleep(5000);
 		Set<String> ids = driver.getWindowHandles();
 		Iterator<String> it = ids.iterator();
 		String parentid = it.next();
 		String childid = it.next();
+	    driver.switchTo().window(childid);
+		fc.writeReview().click();
+		
 		driver.switchTo().window(childid);
-		
-		
-		
+		fc.rating();
+		fc.ratingpoint().click();
 		
 }
 }
